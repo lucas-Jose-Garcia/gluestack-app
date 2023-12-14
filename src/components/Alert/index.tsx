5de8fc7150371b14151239680b99b4ac848b6ffe
lucas-Jose-Garcia <lucasjosegarcia01@gmail.com>
2023-12-13 21:01:53 -0300

    Criação do AlertBox

---
+++ src/components/Alert/index.tsx
@@ -0,0 +1,17 @@
+import { AlertIcon, AlertText, InfoIcon, Alert } from "@gluestack-ui/themed";
+
+interface AlertBoxProps {
+  variant: "solid" | "outline" | "accent"
+  message: string
+}
+
+export function AlertBox({variant, message}: AlertBoxProps) {
+    return (
+      <Alert mx='$2.5' m="$4"  action="info" variant={variant}>
+        <AlertIcon as={InfoIcon} mr="$3"  />
+        <AlertText>
+          {message}
+        </AlertText>
+      </Alert>
+    )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,33 +1,36 @@
-
-
 import { Button, Text } from "@gluestack-ui/themed";
+import { AlertBox } from "components/Alert";
 import { WarningModal } from "components/Dialog";
 import { Layout } from "components/Layout";
 import { useState } from "react";
 
 export function Home() {
-    const [showAlertDialog, setShowAlertDialog] = useState<boolean>(false)
-    return (
-      <Layout>
-        <Text>Kit inicial</Text>
-        <Button
-            onPress={() => setShowAlertDialog(true)}
-            mt="$4"
-        >
-            <Text color="$textLight0">Abrir alerta</Text>
-        </Button>
+  const [showAlertDialog, setShowAlertDialog] = useState<boolean>(false);
+  return (
+    <Layout>
+      <Text size="lg" bold>
+        Kit inicial
+      </Text>
 
-        <WarningModal 
-          show={showAlertDialog}
-          setShow={setShowAlertDialog}
-          title="Mensagem de alerta"
-          message="Este é um exemplo de um AlertDialog aplicado a um componente."
-          button={{
-            title: "Apagar",
-            action: () => setShowAlertDialog(false)
-          }}
-        />
+      <AlertBox
+        variant="accent"
+        message="Exemplo de mensagem: Atualizamos nossos termos de serviço. Revise e aceite para continuar usando nosso serviço."
+      />
 
-      </Layout>
-    )
+      <Button onPress={() => setShowAlertDialog(true)} m="$4">
+        <Text color="$textLight0">Abrir alerta</Text>
+      </Button>
+
+      <WarningModal
+        show={showAlertDialog}
+        setShow={setShowAlertDialog}
+        title="Mensagem de alerta"
+        message="Este é um exemplo de um AlertDialog aplicado a um componente."
+        button={{
+          title: "Apagar",
+          action: () => setShowAlertDialog(false),
+        }}
+      />
+    </Layout>
+  );
 }
